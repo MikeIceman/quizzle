12826513ce96e29ed19e0cdc9796402d6f187c25
Iceman <jmikle@mail.ru>
2018-09-24 04:12:04 +0200

    Games section added

---
+++ src/AppBundle/Controller/GamesController.php
@@ -0,0 +1,72 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: Iceman
+	 * Date: 24.09.2018
+	 * Time: 3:27
+	 */
+
+	namespace AppBundle\Controller;
+
+	use Symfony\Component\Routing\Annotation\Route;
+	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+	use Symfony\Component\HttpFoundation\Request;
+	use AppBundle\Entity as Entity;
+
+	/**
+	 * Class GamesController
+	 * @package AppBundle\Controller
+	 * @Route("/games")
+	 */
+	class GamesController extends Controller
+	{
+		/**
+		 * @Route("/", name="games")
+		 */
+		public function indexAction(Request $request)
+		{
+			return $this->render('games/index.html.twig', [
+				'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+			]);
+		}
+
+		/**
+		 * @Route("/winwheel/", name="games_winwheel")
+		 */
+		public function winwheelAction(Request $request)
+		{
+			return $this->render('games/index.html.twig', [
+				'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+			]);
+		}
+
+		/**
+	 * @Route("/roulette/", name="games_roulette")
+	 */
+		public function rouletteAction(Request $request)
+		{
+			return $this->render('games/index.html.twig', [
+				'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+			]);
+		}
+
+		/**
+		 * @Route("/slots/", name="games_slots")
+		 */
+		public function slotsAction(Request $request)
+		{
+			return $this->render('games/index.html.twig', [
+				'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+			]);
+		}
+
+		/**
+		 * @Route("/lotto/", name="games_lotto")
+		 */
+		public function lottoAction(Request $request)
+		{
+			return $this->render('games/index.html.twig', [
+				'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+			]);
+		}
+	}
