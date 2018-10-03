8770ddc6ed6f0b0db3c3c24bd447cea93f3c35bd
Iceman <jmikle@mail.ru>
2018-10-04 00:31:09 +0200

    Winnings and prizes

--- src/AppBundle/Controller/PersonalController.php
+++ src/AppBundle/Controller/PersonalController.php
@@ -11,6 +11,9 @@
 	use Symfony\Component\Routing\Annotation\Route;
 	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 	use Symfony\Component\HttpFoundation\Request;
+	use AppBundle\Entity\WinWheelSpin;
+	use AppBundle\Entity\UserPrize;
+	use AppBundle\Entity\Operation;
 
 
 	/**
@@ -42,8 +45,22 @@
 		 */
 		public function winningsAction(Request $request)
 		{
+			$user = $this->get('security.token_storage')->getToken()->getUser();
+
+			$repository = $this->getDoctrine()->getRepository(WinWheelSpin::class);
+
+			// Get latest spin
+			$winnings = $repository->findBy(
+				[
+					'user' => $user,
+					'prizeType' => ['cash', 'bonus'],
+				],
+				['id' => 'DESC']
+			);
+
 			return $this->render('personal/winnings.html.twig', [
 				'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+				'winnings' => $winnings
 			]);
 		}
 
@@ -52,8 +69,44 @@
 		 */
 		public function prizesAction(Request $request)
 		{
+			$user = $this->get('security.token_storage')->getToken()->getUser();
+
+			$repository = $this->getDoctrine()->getRepository(UserPrize::class);
+
+			// Get latest spin
+			$prizes = $repository->findBy(
+				[
+					'user' => $user,
+				],
+				['id' => 'ASC']
+			);
+
 			return $this->render('personal/prizes.html.twig', [
 				'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+				'prizes' => $prizes
+			]);
+		}
+
+		/**
+		 * @Route("/operations/", name="personal_operations")
+		 */
+		public function operationsAction(Request $request)
+		{
+			$user = $this->get('security.token_storage')->getToken()->getUser();
+
+			$repository = $this->getDoctrine()->getRepository(Operation::class);
+
+			// Get latest spin
+			$operations = $repository->findBy(
+				[
+					'user' => $user,
+				],
+				['id' => 'ASC']
+			);
+
+			return $this->render('personal/operations.html.twig', [
+				'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+				'operations' => $operations,
 			]);
 		}
 
